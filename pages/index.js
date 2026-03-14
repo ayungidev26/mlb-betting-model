a979d51b9f7cc0029dd35b3694c1f2444c34dca5
ayungidev26 <adamyunginger@gmail.com>
2026-03-14 13:57:30 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:40}}>
+      <h1>MLB Betting Model Dashboard</h1>
+      <p>Serverless MLB analytics running on Vercel.</p>
+    </div>
+  );
+}
