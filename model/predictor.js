509f89091b0eafe078e07c766e9d90bfc329c984
ayungidev26 <adamyunginger@gmail.com>
2026-03-15 14:30:15 -0400

    Update predictor.js

--- model/predictor.js
+++ model/predictor.js
@@ -1,14 +1,18 @@
-export function runPrediction(game){
+import { redis } from "../lib/upstash"
 
-const homeAdvantage = 0.54
+export async function predictGame(game) {
 
-const randomFactor = Math.random() * 0.1
+  const ratings = await redis.get("mlb_team_ratings")
 
-const probability = homeAdvantage + randomFactor
+  const homeRating = ratings[game.homeTeam] || 1500
+  const awayRating = ratings[game.awayTeam] || 1500
 
-return {
-probability,
-edge: probability - 0.5
-}
+  const homeProb =
+    1 / (1 + Math.pow(10, (awayRating - homeRating) / 400))
+
+  return {
+    homeWinProb: homeProb,
+    awayWinProb: 1 - homeProb
+  }
 
 }
